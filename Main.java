21b670c603bbfe7b3a44141a4d790b80174717dd
emredncer <108138943+emredncer@users.noreply.github.com>
2023-09-11 15:23:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         double height;
         double weight;
+        
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter your height in meters: ");
         height = input.nextDouble();
