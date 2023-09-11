fc17bd03df2d64fbe153d930a800e7ccecd8a95b
emredncer <108138943+emredncer@users.noreply.github.com>
2023-09-11 15:10:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double height;
+        double weight;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your height in meters: ");
+        height = input.nextDouble();
+        System.out.print("Please enter your body weight in kg: ");
+        weight = input.nextDouble();
+        //burada index adında bir değişken tanımlamadım çünkü daha sonra kullanmayacağım.
+        System.out.println("Your body mass index is: "+ ((weight)/(height*height)));
+    }
+}
